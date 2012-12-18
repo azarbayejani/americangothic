204c32e049ac0cda705d374a69bfcda6f4eeb4a2
Bobak Azarbayejani <azar@relic.(none)>
2012-12-18 11:28:50 -0500

    Fixes in review.rb (and associated changes elsewhere)

--- lib/americangothic/review.rb
+++ lib/americangothic/review.rb
@@ -1,61 +1,83 @@
 require 'nokogiri'
 require 'open-uri'
 
-class Review 
+module AmericanGothic
+	class Review 
 
-	#private_class_method :new
-	attr_accessor :title,:text
+		#private_class_method :new
+		attr_accessor :title,:text
 
-	def initialize (album_title,album_artist,artwork,rating,reviewer,date,release_date,label,text)
-		@title = album_title
-		@artist = album_artist
-		@artwork = artwork
-		@rating = rating
-		@reviewer = reviewer
-		@date = date
-		@release_date = release_date
-		@label = label
-		@text = text
-	end
-
-	def self.new_from_url (url)
-
-		if has_local_cache? url
-			new_from_local url
+		def initialize opts 
+			@title = opts[:title] 
+			@artist = opts[:artist] 
+			@artwork = opts[:artwork] 
+			@rating = opts[:rating] 
+			@reviewer = opts[:reviewer]
+			@date = opts[:date]
+			@release_date = opts[:release_date]
+			@label = opts[:label]
+			@text = opts[:text] 
 		end
 
-		#puts url
-		begin 
-			review = Nokogiri::HTML(open(url))
-		rescue Interrupt, Errno::EINTR, Errno::ETIMEDOUT
-			retry
+		def self.new_from_url (url)
+
+			#puts url
+			begin 
+				review = Nokogiri::HTML(open(url))
+			rescue Interrupt, Errno::EINTR, Errno::ETIMEDOUT
+				retry
+			end
+
+			artist = review.css('.info h1')[0]
+			title = artist.next_element
+			release_info = title.next_element
+			reviewer_info = release_info.next_element
+
+			artwork_markup = review.css('.artwork > img')[0]
+			if artwork_markup then artwork = artwork_markup['src'] end
+
+			if reviewer_info then author, publish_date = reviewer_info.text.split(';') end
+			if release_info then label , year = release_info.text.split(';') end
+			#if score then score = score.text end
+
+			text = review.css('.editorial').text.gsub(/\n/,"").delete('-')
+
+			opts = { 
+				:title => title.text,
+				:artist => artist.text,
+				:artwork => artwork,
+				#:score => score,
+				:reviewer => author,
+				:date => publish_date,
+				:release_date => year,
+				:label => label,
+				:text => text
+			}
+
+			self.new(opts)
+
 		end
 
-		artist = review.css('.info h1')[0]
-		title = artist.next_element
-		release_info = title.next_element
-		reviewer_info = release_info.next_element
+		def self.new_from_local url
 
-		artwork_markup = review.css('.artwork > img')[0]
-		if artwork_markup then artwork = artwork_markup['src'] end
+		end
 
-		if reviewer_info then author, publish_date = reviewer_info.text.split(';') end
-		if release_info then label , year = release_info.text.split(';') end
-		if score then score = score.text end
+		def self.has_local_cache? url
+			expectedfilename = url.split("/")[-1]
+			return File.exists? "#{AmericanGothic::DATADIR}/#{expectedfilename}"
+		end
 
-		text = review.css('.editorial').text.gsub(/\n/,"").delete('-')
-
-		new(title.text,artist.text,artwork,score,author,publish_date,year,label,text)
+		def to_json(*a)
+			{
+				"title" => @title,
+				"artist" => @artist,
+				"artwork" => @artwork,
+				"rating" => @rating,
+				"reviewer" => @reviewer,
+				"label" => @label,
+				"text" => @text
+			}.to_json(*a)
+		end
 
 	end
-
-	def self.new_from_local url
-
-	end
-
-	def self.has_local_cache? url
-		expectedfilename = url.split("/")[-1]
-		return File.exists? "#{AmericanGothic::DATADIR}/#{expectedfilename}"
-	end
-
 end
